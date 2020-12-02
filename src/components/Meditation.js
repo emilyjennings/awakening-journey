26ab27e8f806935e11da22a57f06b7c0d8d68811
github username <github email address>
2020-12-01 21:12:29 -0500

    meditation

--- src/components/Meditation.js
+++ src/components/Meditation.js
@@ -2,7 +2,8 @@
 function Meditation() {
   return (
     <div className="meditation">
-      <p>This page is developing, please check back later for resources about meditation. (November 24, 2020)</p>
+      <p>Meditation is the key to unleashing your potential. It allows us to go beyond our limitations such as stress and fear, and see the beauty and purpose in reality. I will be adding techniques and commentary here in coming months.</p>
+      <p>This page is developing, so please check back later for resources about meditation. (Last updated: December 2020)</p>
     </div>
 
   );
