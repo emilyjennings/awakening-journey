4d8ef9a7f1315d3caeee600f5267a7a95b28a116
github username <github email address>
2020-12-16 07:45:01 -0500

    body

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,9 +11,9 @@ function Body() {
         <div className="landing-message">
           <h3>You are not alone.</h3>
           <p>Isn't it beautiful that life has brought you here?</p>
-          <p>You've come to the right place to find out how to understand your inner self and to connect with others experiencing awakening.</p>
+          <p>You've come to the right place if you are feeling the call to arrive at a higher version of yourself.</p>
           <p>This resource for your awakening is growing and expanding, just like we are. Sign up for my newsletter for updates!</p>
-          <p><a href="https://join.slack.com/t/waking-upworkspace/shared_invite/zt-izfp06jo-j0jDznPIrbUseT_RCyXn~Q">Join our slack community</a> to connect with a others around consciousness and awakening.</p>
+          <p><a href="https://join.slack.com/t/waking-upworkspace/shared_invite/zt-izfp06jo-j0jDznPIrbUseT_RCyXn~Q">Join our slack community</a> to connect with others about awakening.</p>
         </div>
         <div className="landing-message">
           <div className="lotus-img">
