524d8876c672de6d10b70da3e90210af65464819
github username <github email address>
2020-12-16 19:59:50 -0500

    subscribe form

--- src/components/EmailListForm.js
+++ src/components/EmailListForm.js
@@ -1,56 +1,40 @@
 import React, { Component } from 'react';
 
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
+// const encode = (data) => {
+//   return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&");
+// }
 
 export default class EmailListForm extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { email: "", firstname: ""};
-  }
-
-
-  // Code for the contact form submission given by Netlify documentation
-  handleSubmit = e => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "emaillist", ...this.state })
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { email: "", firstname: ""};
+  // }
+  //
+  //
+  // // Code for the contact form submission given by Netlify documentation
+  // handleSubmit = e => {
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "emaillist", ...this.state })
+  //   })
+  //     .then(() => alert("Success!"))
+  //     .catch(error => alert(error));
+  //
+  //   e.preventDefault();
+  // };
+  //
+  // handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { firstname, email } = this.state;
     return (
       <div className="email-list-form">
         <div className="contactcontainer">
-          <div className="emailformcaption">Subscribe to my newsletter for a free 4-step meditation guide!</div>
-          <form name="emaillist" onSubmit={this.handleSubmit} data-netlify="true" data-netlify-honeypot="bot-field">
-            <input type="hidden" name="form-name" value="emaillist" />
-            <div className="input-row-1">
-                <label>
-                  First Name: <br></br> <input type="firstname" name="firstname" value={firstname} onChange={this.handleChange} />
-                </label>
-            </div>
-            <div className="input-row-1">
-                <label>
-                  Email: <br></br> <input type="email" name="email" value={email} onChange={this.handleChange} />
-                </label>
-            </div>
-
-              <button id="submit" type="submit">Send</button>
-
-          </form>
+          <div className="emailformcaption">Subscribe to my newsletter for a free 4-step Meditation Guide!</div>
+          <a href="https://f01b0c2b.sibforms.com/serve/MUIEAJ6sL4tVXfn72iaXm-kwyoX8x-aFlrNqg8KLr46JpLePOxHNxqnvkgDH9rkvB1Bv8NMELRnt9PeIKFE6IaJLbBHBCQLMgG4AGaaDBToh2j_iJsii1goNS-QZPjT7C0Oy0mhYRNCnJteTOVhKcYPJoxSCTi-5W9NS5RXto-eGjcB0ShculqReXH6W_OPwRFsOqo1XTHRkYNLd">Click here to sign up!</a>
         </div>
       </div>
     );
