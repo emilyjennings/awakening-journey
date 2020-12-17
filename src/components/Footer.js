a2e9c605e3e38a1803cf0cfe066fa5a76896649e
github username <github email address>
2020-12-16 20:36:28 -0500

    form subscribe and styling

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <div className="footer-box">
+        <p>© Designed and Built by Emily Jennings 2020</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default Footer;
