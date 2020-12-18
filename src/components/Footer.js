451d9236a2faa44933e25c874c28246cb6a672c2
github username <github email address>
2020-12-18 08:36:56 -0500

    styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <div className="Footer">
       <div className="footer-box">
-        <p><a href="www.this-is-emily.com">© Designed and Built by Emily Jennings 2020</a></p>
+        <p><a href="https://www.this-is-emily.com">© Designed and Built by Emily Jennings 2020</a></p>
       </div>
     </div>
 
