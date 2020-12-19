3c0b16a792db15eb44bd880bb64f3d732ee69d9a
github username <github email address>
2020-12-18 23:40:18 -0500

    sign up

--- src/components/EmailListForm.js
+++ src/components/EmailListForm.js
@@ -8,9 +8,9 @@ export default class EmailListForm extends Component {
     return (
       <div className="email-list-form">
         <div className="contactcontainer">
-          <div className="emailformcaption">Subscribe to my newsletter for a free 4-step Meditation Guide!</div>
+          <div className="emailformcaption">Subscribe to my newsletter, Mindful Body, and get your free 4-step Meditation Guide</div>
           <div class="space"></div>
-          <a href="https://f01b0c2b.sibforms.com/serve/MUIEAEPafAzc4WOTqCB086fU3JssUUJ5a9d3MNEgUbxFZi-A_TOM7nQ_q_Z4La0QPxZP4xtDawKcTIaOe_IOTWEOGsTORriqLjvfLspcnsIyZnUou4dVut1S7PHlZoBKHDPw7gmEfxWDwEk_kzg8iROiBaTbo_HQgGh31kPqMfZnr6ou3o3wrwrbR8UTlA1C6DPc4yACtu6ktVlc">Click here to sign up!</a>
+          <a href="https://f01b0c2b.sibforms.com/serve/MUIEAEPafAzc4WOTqCB086fU3JssUUJ5a9d3MNEgUbxFZi-A_TOM7nQ_q_Z4La0QPxZP4xtDawKcTIaOe_IOTWEOGsTORriqLjvfLspcnsIyZnUou4dVut1S7PHlZoBKHDPw7gmEfxWDwEk_kzg8iROiBaTbo_HQgGh31kPqMfZnr6ou3o3wrwrbR8UTlA1C6DPc4yACtu6ktVlc">Sign up here!</a>
         </div>
       </div>
     );
