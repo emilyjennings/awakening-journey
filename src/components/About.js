520f047c9a10034a6b1b9dfcaa9915e5c2be5749
github username <github email address>
2020-12-18 23:38:05 -0500

    image swap

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import FadeIn from 'react-fade-in';
 
-import temple from '../images/temple.jpg'
+import profile from '../images/crown.jpg'
 import logo from '../images/LogoLight.png'
 import { Link } from "react-router-dom";
 
@@ -14,8 +14,8 @@ function About() {
       <p>There is community here in our <a href="https://join.slack.com/t/waking-upworkspace/shared_invite/zt-izfp06jo-j0jDznPIrbUseT_RCyXn~Q"><b>slack channel</b></a>, and there are resources to help you in the pages for <a href="https://awakening-journey.medium.com"><b>Blog</b></a> and <Link to="/meditation"><b>Meditation</b></Link>.</p>
       <p>If you are feeling a call to look closer at reality and identify your deeper self as well as your purpose, you are not alone. You are among friends here.</p>
       <FadeIn>
-        <div className="temple">
-          <img src={temple} alt="" />
+        <div className="profile">
+          <img src={profile} alt="" />
         </div>
       </FadeIn>
       <FadeIn>
@@ -26,7 +26,7 @@ function About() {
       <FadeIn>
         < EmailListForm />
       </FadeIn>
-      <img src={logo} alt=""/>
+      <div class="lotus-img"><img src={logo} alt=""/></div>
     </div>
 
   );
