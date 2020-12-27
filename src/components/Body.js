a895b54a78c56b426efde24770df9c4d10c288fb
github username <github email address>
2020-12-27 17:21:26 -0500

    updates

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,7 +10,7 @@ function Body() {
       <div className="home-message">
         <div className="landing-message">
           <h3>You are not alone.</h3>
-          <p class="new">This website is the old version and will be retired soon - <a href="www.shaktimindfulawakening.com">check me out at the new site here!</a></p>
+          <p class="new">This website is the old version and will be retired soon - <a href="https://www.shaktimindfulawakening.com">check me out at the new site here!</a></p>
           <p>Isn't it beautiful that life has brought you here?</p>
           <p>You've come to the right place if you are feeling the call to arrive at a higher version of yourself.</p>
           <p><a href="https://join.slack.com/t/waking-upworkspace/shared_invite/zt-kc17udt1-4R6tHNHljVMX9qxKc8OiYg">Join our slack community</a> to connect with others about awakening.</p>
